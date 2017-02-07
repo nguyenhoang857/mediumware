f48d4ea9fd9073e4123d2ca701e5101b2ac5f801
Hoang Nguyen <Hoang_Nguyen@brown.edu>
2017-02-06 21:34:37 -0500

    init

---
+++ src/platforms/desktop/root/desktop/gui.js
@@ -0,0 +1,70 @@
+IDE_Morph.prototype.originalOpenIn = IDE_Morph.prototype.openIn;
+IDE_Morph.prototype.openIn = function (world) {
+    this.originalOpenIn(world);
+    this.checkForNewVersion();
+};
+
+IDE_Morph.prototype.checkForNewVersion = function () {
+    var myself = this,
+        latest = this.getURL('http://snap4arduino.org/downloads/LATEST'),
+        current = this.version();
+
+    function outdatedVersion (current,latest) {
+        var current = current.split('.'),
+            latest = latest.split('.'),
+            versionLength = Math.max(current.length, latest.length);
+
+        for (var i = 0; i < versionLength; i++) {
+            current[i] = Number(current[i]) || 0;
+            latest[i] = Number(latest[i]) || 0;
+            if (current[i] < latest[i]) return true;
+            if (current[i] > latest[i]) return false;
+        }
+        return false;
+    };
+
+    if (outdatedVersion(current,latest)) {
+        this.confirm(
+            'A new version of Snap4Arduino has been released: ' 
+                + latest 
+                + '\nDo you wish to download it?',
+            'New version available',
+            function () {
+                myself.downloadVersion(latest);
+            }
+        );
+    }
+};
+
+IDE_Morph.prototype.downloadVersion = function (versionName) {
+    var os = this.osName();
+
+    nw.Shell.openExternal(
+        'http://snap4arduino.org/downloads/'
+            + versionName 
+            + '/Snap4Arduino_desktop-'
+            + os
+            + '-'
+            + this.arch()
+            + '_'
+            + versionName
+            + (os === 'gnu' ? '.tar.gz' : '.zip')
+    );
+};
+
+IDE_Morph.prototype.osName = function () {
+    var platform = require('os').platform(),
+        platformDict = {
+            'linux' : 'gnu',
+            'win32' : 'win',
+            'darwin': 'osx'
+        };
+
+    return platformDict[platform];
+};
+
+IDE_Morph.prototype.arch = function () {
+    return ((require('os').arch() === 'x64' || process.env.hasOwnProperty('PROCESSOR_ARCHITEW6432'))
+            ? 64 
+            : 32);
+};
---
+++ src/platforms/embedded/cli/root/linino/gui.js
@@ -0,0 +1,1 @@
+../../../../web/chromium/root/plugin/gui.js
